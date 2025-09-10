43c5ac0ba2db4fa26b8d536396b8f79790db0f5f
Kashyap Maddala <kashyap.maddala@gmail.com>
2025-09-10 12:36:29 -0400

    a commit

--- data_to_train_classification_extraction_v1.py
+++ data_to_train_classification_extraction_v1.py
@@ -1,6 +1,8 @@
 import kagglehub
 
 # Download latest version
+## This downloads to computer files
 path = kagglehub.dataset_download("paramaggarwal/fashion-product-images-small")
-
 print("Path to dataset files:", path)
+
+
